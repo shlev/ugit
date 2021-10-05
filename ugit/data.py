432ee8c87d06ef23c91cd069d9a66d01d489ce82
shlomi <shl.lev.ks@gmail.com>
2021-10-05 12:53:59 +0300

    init ugit

---
+++ ugit/data.py
@@ -0,0 +1,40 @@
+import hashlib
+import os
+import readline
+
+GIT_DIR = '.ugit'
+
+def init():
+    os.makedirs (GIT_DIR)
+    os.makedirs(f'{GIT_DIR}/objects')
+
+
+def set_HEAD(oid):
+    with open(f'{GIT_DIR}/HEAD', 'w') as f:
+        f.write(oid)
+
+
+def get_HEAD():
+    if os.path.isfile(f'{GIT_DIR}/HEAD'):
+        with open (f'{GIT_DIR}/HEAD') as f:
+            return f.read().strip()
+
+
+def hash_object(data, type_='blob'):
+    obj = type_.encode() + b'\x00' + data
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(obj)
+    return oid
+
+
+def get_object(oid, expected='blob'):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        obj = f.read()
+
+    type_, _, content = obj.partition(b'\x00')
+    type_ = type_.decode()
+
+    if expected is not None:
+        assert type_ == expected, f'Expected {expected}, got{type_}'
+    return content
